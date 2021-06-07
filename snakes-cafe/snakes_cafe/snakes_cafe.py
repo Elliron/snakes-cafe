0f9e7954afd0d56fb7d230bc58bf752fb6082c33
Glenn <Elliron.Clark@gmail.com>
2021-06-06 23:17:28 -0700

    created lab-1 snakes cafe

---
+++ snakes-cafe/snakes_cafe/snakes_cafe.py
@@ -0,0 +1,68 @@
+starsintro = "*" * 38
+print(starsintro)
+
+welcome = "**" + "    " + "Welcome to the Snakes Cafe!" + "    " + "**"
+print(welcome)
+
+please = "**" + "    " + "Please see our menu below." + "    " + "**"
+print(please)
+
+twostar = "**"
+print(twostar)
+
+toquit = "**" + "    " + 'To quit at any time, type "quit"' + "    " + "**"
+print(toquit)
+
+print(starsintro)
+print()
+apps = "Appetizers"
+print(apps)
+hyphens = "-" * 10
+print(hyphens)
+applist = []
+applist.append("Wings")
+applist.append("Cookies")
+applist.append("Spring Rolls")
+print(applist[0]) 
+print(applist[1]) 
+print(applist[2]) 
+print()
+entrees = "Entrees"
+print(entrees)
+hyphsev = "-" * 7
+print(hyphsev)
+entreelist = []
+entreelist.append("Salmon")
+entreelist.append("Steak")
+entreelist.append("Meat Tornado")
+entreelist.append("A Literal Garden")
+print(entreelist[0])
+print(entreelist[1])
+print(entreelist[2])
+print(entreelist[3])
+print()
+dessert = "Desserts"
+hyphten = "-" * 10
+dessertlist = []
+dessertlist.append("Ice Cream")
+dessertlist.append("Cake")
+dessertlist.append("Pie")
+print(dessertlist[0])
+print(dessertlist[1])
+print(dessertlist[2])
+print()
+drink = "Drinks"
+print(drink)
+hyphsix = "-" * 6
+drinklist = []
+drinklist.append("Coffee")
+drinklist.append("Tea")
+drinklist.append("Unicorn Tears")
+print(drinklist[0])
+print(drinklist[1])
+print(drinklist[2])
+print()
+print(starsintro)
+what = twostar + " " + "What would you like to order?" + " " + twostar
+print(what)
+print(starsintro)
